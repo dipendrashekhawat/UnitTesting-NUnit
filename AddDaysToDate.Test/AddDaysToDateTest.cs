0c2bf0bd7f2d7e06e6912ff92c6bb47644789b26
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-08-09 09:43:04 +0530

    Implementation of Unit testing using NUnit

---
+++ AddDaysToDate.Test/AddDaysToDateTest.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using AddDaysToDate;
+using System;
+
+namespace Tests
+{
+    public class AddDaysToDateTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void NumberOfDays_WhenNumberOfDaysAre240_ReturnNewDate()
+        {
+            //Arrange
+            var givenDate = Convert.ToDateTime("05-01-2019");
+            var numberOfDays = 240;
+
+            //Act
+            var result = Program.NumberOfDays(numberOfDays, givenDate);
+
+            //Assert
+            Assert.AreEqual(Convert.ToDateTime("02-09-2019"),result);
+        }
+
+    }
+}
---
+++ AddDaysToDate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AddDaysToDate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter date in the format dd-mm-yyyy");
+                DateTime inputDate = Convert.ToDateTime(Console.ReadLine());
+
+                Console.WriteLine("Enter the number of days you want to add to the above date: ");
+                int numberOfDays = Convert.ToInt32(Console.ReadLine());
+
+                DateTime newDate = NumberOfDays(givenDate: inputDate, days: numberOfDays);
+
+                Console.WriteLine("New date after adding {0} days to {1} is: {2} ", numberOfDays, inputDate, newDate.ToShortDateString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while adding days :" + ex.Message);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Add number of days to a given date
+        /// </summary>
+        /// <param name="days">Number of days</param>
+        /// <param name="givenDate">Date in mm-dd-yyyy format</param>
+        /// <returns>New date with the days added</returns>
+        public static DateTime NumberOfDays(int days, DateTime givenDate)
+        {
+            DateTime newDate = givenDate.AddDays(days);
+            return newDate;
+        }
+
+    }
+}
