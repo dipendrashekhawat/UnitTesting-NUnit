ad5ed8abb88252544097d211aea9e2ad52b2d324
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-08-09 10:09:47 +0530

    Added a unit test for numbers of days are less than zero

--- AddDaysToDate.Test/AddDaysToDateTest.cs
+++ AddDaysToDate.Test/AddDaysToDateTest.cs
@@ -25,5 +25,18 @@ namespace Tests
             Assert.AreEqual(Convert.ToDateTime("02-09-2019"),result);
         }
 
+        [Test]
+        public void NumberOfDays_WhenNumberOfDaysAreLessThanZero_ShouldReturnDateLessThanGivenDate()
+        {
+            //Arrange
+            var givenDate = Convert.ToDateTime("05-01-2019");
+            var numberOfDays = -7;
+
+            //Act
+            var result = Program.NumberOfDays(numberOfDays, givenDate);
+
+            //Assert
+            Assert.That(result, Is.LessThan(givenDate));
+        }
     }
 }
